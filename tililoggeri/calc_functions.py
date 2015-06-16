5a3b0b1dc9b79223728481ac443257802d87831a
nikulaj <aJns@users.noreply.github.com>
2015-06-16 01:30:50 +0000

    derp di durp

--- tililoggeri/calc_functions.py
+++ tililoggeri/calc_functions.py
@@ -2,7 +2,7 @@ def calculate_sum(transaction_list):
     sum_float = 0.0
     for transaction in transaction_list:
         sum_float += float(transaction["Määrä"].replace(',','.'))
-        print(sum_float)
+        print("{0:.2f}".format(sum_float) + "\t:\t" + transaction["Saaja/Maksaja"])
 
 
 
