f02a4db6e9d03198251ae149a657dcc5b563e694
nikulaj <aJns@users.noreply.github.com>
2015-06-20 14:15:54 +0300

    Added the lua main file

---
+++ tililoggeri.lua
@@ -0,0 +1,15 @@
+local instructions = require "parse_instructions"
+
+
+function main()
+    print("Enter account transactions file name: ")
+    fname = io.read()
+    print("Enter file format, eg. bank: ")
+    fformat = io.read()
+end
+
+function parse_file(instructions, filename, fileformat, transactions)
+
+end
+
+main()
