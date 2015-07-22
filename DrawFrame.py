69b68e015a428e328e93470b3899f7c80093176f
nikulaj <aJns@users.noreply.github.com>
2015-07-22 13:53:29 +0300

    Looking better

--- DrawFrame.py
+++ DrawFrame.py
@@ -1,6 +1,11 @@
 from tkinter import Canvas, Frame, BOTH
 from datetime import datetime, timedelta
 
+BAR_WIDTH = 10
+BAR_SPACING = 5
+VERTICAL_BIAS = 200
+SUM_GAIN = -0.25
+
 class DrawFrame(Frame):
 
     def __init__(self, parent, transaction_table):
@@ -25,13 +30,16 @@ class DrawFrame(Frame):
             month = date.month
             day = date.day
 
-            xCoord1 = 30 * i
-            xCoord2 = 30 * i + 20
-            yCoord1 = 10
+            xCoord1 = BAR_SPACING * i
+            xCoord2 = BAR_SPACING * i + BAR_WIDTH
+            yCoord1 = VERTICAL_BIAS
             yCoord2 = self.trans_table.get_sum(year, month, day)
 
             if yCoord2 == None: 
                 yCoord2 = yCoord1
+            else:
+                yCoord2 = yCoord2 * SUM_GAIN + yCoord1
+                print(yCoord2)
 
             canvas.create_rectangle(xCoord1, yCoord1, xCoord2, yCoord2, 
                     outline="#fb0", fill="#fb0")
